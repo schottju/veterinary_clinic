fb8c923d85c84aedd2e5b59a10c3c7fa50b51666
Adam Winiarczyk <daktyr@gmail.com>
2015-07-14 13:08:06 +0200

    Add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  enum role: [ :user, :doctor, :admin ]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
