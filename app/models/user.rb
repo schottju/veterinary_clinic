dcd9a17d99e99d4805fcf786486f8c992aabeeb2
Adam Winiarczyk <daktyr@gmail.com>
2015-07-15 17:30:10 +0200

    Add association between user and address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   enum role: [ :patient, :doctor, :admin ]
 
+  has_one :address
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
