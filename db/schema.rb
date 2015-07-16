322bb2ea5de86c7b6a6b43315f6cda06256a786b
Adam Winiarczyk <daktyr@gmail.com>
2015-07-16 11:18:47 +0200

    Add phone number to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715225334) do
+ActiveRecord::Schema.define(version: 20150716091812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -180,6 +180,7 @@ ActiveRecord::Schema.define(version: 20150715225334) do
     t.integer  "pesel"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "phone_number"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
