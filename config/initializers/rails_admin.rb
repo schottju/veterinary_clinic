9267b1e58aaba2366fd64ffb9a235fe41f66c19e
Adam Winiarczyk <daktyr@gmail.com>
2015-07-27 18:19:23 +0200

    Add admin authorization. Add more translations

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -25,11 +25,15 @@ RailsAdmin.config do |config|
 
   ### Popular gems integration
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+
+  config.current_user_method(&:current_user)
+
+  config.authorize_with do
+    (redirect_to main_app.authenticated_root_path, notice: "Brak uprawnień administratora.") unless current_user.admin?
+  end
 
   ## == Cancan ==
   # config.authorize_with :cancan
