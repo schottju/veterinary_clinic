25f6edfdc5dd09292347b2bcb74b0968ec3e2f17
Adam Winiarczyk <daktyr@gmail.com>
2015-08-03 11:31:24 +0200

    Fix active record time zone

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module VeterinaryClinic
     config.active_record.raise_in_transactional_callbacks = true
 
     config.time_zone = 'Warsaw'
-    config.active_record.default_timezone = 'Warsaw'
+    config.active_record.default_timezone = :local
   end
 end
