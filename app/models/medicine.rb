54e2126988e369515ab23e6e06d2c211c53103d8
Adam Winiarczyk <daktyr@gmail.com>
2015-08-13 11:01:48 +0200

    Remove presence validation of dosage from medicines

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -2,7 +2,7 @@ class Medicine < ActiveRecord::Base
   belongs_to :unit
   has_and_belongs_to_many :medical_records
 
-  validates_presence_of :name, :amount, :grace_period, :price, :dosage, :unit_id
+  validates_presence_of :name, :amount, :grace_period, :price, :unit_id
 
   def custom_label_method
     "##{id} #{name}, ilość: #{amount} #{unit.try(:name)}"
